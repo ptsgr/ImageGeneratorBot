f51cab11ea57ef05315f0eb88a2a786833acc31f
ptsgr <egor.pats@gmail.com>
2021-04-03 12:41:04 +0300

    Added the ability to add text to the image

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ptsgr/ImageGeneratorBot
 
 go 1.15
 
-require github.com/spf13/viper v1.7.1
+require (
+	github.com/spf13/viper v1.7.1
+	golang.org/x/image v0.0.0-20190802002840-cff245a6509b
+)
--- pkg/image_creator/image_creator.go
+++ pkg/image_creator/image_creator.go
@@ -2,6 +2,7 @@ package image_creator
 
 import (
 	"image"
+	"image/color"
 	"image/draw"
 	"image/png"
 	"io"
@@ -9,6 +10,9 @@ import (
 
 	"github.com/ptsgr/ImageGeneratorBot/pkg/hex2rgb"
 	"github.com/spf13/viper"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/basicfont"
+	"golang.org/x/image/math/fixed"
 )
 
 type Image struct {
@@ -24,14 +28,17 @@ type ImageProperties struct {
 	TextColor       string
 }
 
+var imageProperties ImageProperties
+
 func (imageProperties *ImageProperties) InitImageProperties() {
 	imageProperties.ImageHeight = viper.GetInt("ImageProperties.ImageHeight")
 	imageProperties.ImageWigth = viper.GetInt("ImageProperties.ImageWigth")
 	imageProperties.BackgraundColor = viper.GetString("ImageProperties.BackgraundColor")
+	imageProperties.Text = viper.GetString("ImageProperties.Text")
+	imageProperties.TextColor = viper.GetString("ImageProperties.TextColor")
 }
 
 func (img *Image) CreateImage(out io.Writer) error {
-	var imageProperties ImageProperties
 	imageProperties.InitImageProperties()
 	img.Image = image.NewRGBA(image.Rect(0, 0, imageProperties.ImageWigth, imageProperties.ImageHeight))
 	clr, err := hex2rgb.ParsingHex(imageProperties.BackgraundColor)
@@ -39,5 +46,22 @@ func (img *Image) CreateImage(out io.Writer) error {
 		log.Fatalf("Error parse color from config: %s", err.Error())
 	}
 	draw.Draw(img.Image, img.Image.Bounds(), image.NewUniform(clr.ToRGB()), image.Point{}, draw.Src)
+	textClr, err := hex2rgb.ParsingHex(imageProperties.TextColor)
+	if err != nil {
+		log.Fatalf("Error parse color from config: %s", err.Error())
+	}
+
+	img.AddText(imageProperties.ImageWigth/2, imageProperties.ImageHeight/2, imageProperties.Text, textClr.ToRGB())
 	return png.Encode(out, img.Image)
+}
+
+func (img *Image) AddText(x, y int, text string, clr color.Color) {
+	point := fixed.Point26_6{X: fixed.Int26_6(x * 64), Y: fixed.Int26_6(y * 64)}
+	d := &font.Drawer{
+		Dst:  img.Image,
+		Src:  image.NewUniform(clr),
+		Face: basicfont.Face7x13,
+		Dot:  point,
+	}
+	d.DrawString(text)
 }
