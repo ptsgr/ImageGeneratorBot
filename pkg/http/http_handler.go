92bf90bfec0494ab78eb66176b6379aa77b54c46
ptsgr <egor.pats@gmail.com>
2021-04-06 10:58:29 +0300

    Added favicon handler

--- pkg/http/http_handler.go
+++ pkg/http/http_handler.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"html"
 	"log"
 	"net/http"
 
@@ -10,11 +11,15 @@ import (
 func InitHandlers() http.Handler {
 	ServeMux := http.NewServeMux()
 	ServeMux.HandleFunc("/", imageHandler)
-
+	ServeMux.HandleFunc("/favicon.ico", faviconHandler)
 	return ServeMux
 }
 
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+}
+
 func imageHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Hello, %q\n", html.EscapeString(r.URL.Path))
 	img := new(image_creator.Image)
 	if err := img.CreateImage(w); err != nil {
 		log.Fatalf("Error imageGenerator running http server: %s", err.Error())
