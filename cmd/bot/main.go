1afc95835e709f758e8fa9c04bd04f7456529ea5
ptsgr <egor.pats@gmail.com>
2021-04-10 10:53:13 +0300

    Added telegram bot main file

---
+++ cmd/bot/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+const telegramTokenEnv string = "TELEGRAM_TOKEN"
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv(telegramTokenEnv))
+	if err != nil {
+		log.Fatalf("Can't connect to telegram API: %s", err.Error())
+	}
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		if !update.Message.IsCommand() {
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+		switch update.Message.Command() {
+		case "help":
+			msg.Text = "Write /status command"
+		case "status":
+			msg.Text = "Ok"
+		default:
+			msg.Text = "Try /help command"
+		}
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Panic(err)
+		}
+	}
+
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ptsgr/ImageGeneratorBot
 go 1.15
 
 require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.0.0-rc1
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	github.com/spf13/viper v1.7.1
 	golang.org/x/image v0.0.0-20190802002840-cff245a6509b
