8a19e1c57a6d98c38610b15e9cc4a0c418ae0b95
ptsgr <egor.pats@gmail.com>
2021-04-29 11:49:36 +0300

    Added the ability to send an image upon user request

--- cmd/bot/bot.go
+++ cmd/bot/bot.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"errors"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -33,20 +36,57 @@ func main() {
 
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-		// msg := tgbotapi.NewPhotoUpload(update.Message.Chat.ID, nil)
 
 		switch update.Message.Command() {
 		case "help":
 			msg.Text = "Write /status command"
+			if _, err := bot.Send(msg); err != nil {
+				log.Panic(err)
+			}
 		case "status":
 			msg.Text = "OK"
+			if _, err := bot.Send(msg); err != nil {
+				log.Panic(err)
+			}
+		case "image":
+			URL := "http://127.0.0.1:9000/img.png"
+
+			photoBytes, err := GetImageFromURL(URL)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			photoFileBytes := tgbotapi.FileBytes{
+				Name:  "picture",
+				Bytes: photoBytes,
+			}
+			bot.Send(tgbotapi.NewPhotoUpload(update.Message.Chat.ID, photoFileBytes))
 		default:
 			msg.Text = "Try /help command"
+			if _, err := bot.Send(msg); err != nil {
+				log.Panic(err)
+			}
 		}
 
-		if _, err := bot.Send(msg); err != nil {
-			log.Panic(err)
-		}
 	}
 
+}
+
+func GetImageFromURL(URL string) ([]byte, error) {
+	response, err := http.Get(URL)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		return nil, errors.New("Received non 200 response code")
+	}
+
+	photoBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	return photoBytes, nil
 }
